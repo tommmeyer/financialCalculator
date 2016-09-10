5921ddfd527e5d0b1e8d1de0b387edebaac35cd9
tommeyer <tommeyer@toms-mbp.home>
2016-09-09 22:31:01 -0400

    limits entries to real life numbers and debugs unrealistic answers

--- financial calculator/financialCalculator/src/financialCalculator.java
+++ financial calculator/financialCalculator/src/financialCalculator.java
@@ -19,16 +19,82 @@ public class financialCalculator {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Enter the expected social secuity:  ");
 		double socialSecurity = input.nextDouble();
+		byte x = 0;
+		while (x==0){
+			if (socialSecurity >= 0 && socialSecurity <=2642){
+				x++;
+			}
+			else{
+				System.out.print("Invalid value. Enter valid social secuity amount:  ");
+				socialSecurity = input.nextDouble();
+			}
+		}
 		System.out.print("Enter the expected years retired:  ");
 		double yearsRetired = (input.nextDouble()*12);
+		byte y = 0;
+		while (y==0){
+			if (yearsRetired >= 0){
+				y++;
+			}
+			else{
+				System.out.print("Invalid value. Enter valid number of years retired:  ");
+				yearsRetired = input.nextDouble();
+			}
+		}	
 		System.out.print("Enter the expected interest rate when retired (1 corresponds to 1%- enter a number between 0 and 3):  ");
-		double retiredRate = (input.nextDouble())/(100*12);
+		double retiredRate = input.nextDouble();
+		byte z = 0;
+		while (z==0){
+			if (retiredRate >= 0 && retiredRate <=3){
+				z++;
+			}
+			else{
+				System.out.print("Invalid value. Enter valid retired interest rate:  ");
+				retiredRate = input.nextDouble();
+			}
+		}
+		retiredRate = retiredRate/1200;
 		System.out.print("Enter the expected monthly income needed in retiremnt:  ");
-		double requiredIncome = (input.nextDouble() - socialSecurity);
+		double requiredIncome = input.nextDouble();
+		byte a = 0;
+		while (a==0){
+			if (requiredIncome >= 0){
+				a++;
+			}
+			else{
+				System.out.print("Invalid value. Enter valid required income:  ");
+				requiredIncome = input.nextDouble();
+			}
+		}
+		requiredIncome = requiredIncome - socialSecurity;
+		if( requiredIncome < 0){
+			requiredIncome = 0;
+		}
 		System.out.print("Enter the years expected to work:  ");
 		double yearsToWork = (input.nextDouble()*12);
+		byte b = 0;
+		while (b==0){
+			if (yearsToWork >= 0){
+				b++;
+			}
+			else{
+				System.out.print("Invalid value. Enter valid required income:  ");
+				yearsToWork = input.nextDouble();
+			}
+		}
 		System.out.print("Enter the expected interest rate when working (1 corresponds to 1%- enter a number between 0 and 20):  ");
-		double interestRate = (input.nextDouble()/(100*12));
+		double interestRate = input.nextDouble();
+		byte c = 0;
+		while (c==0){
+			if (interestRate >= 0 && interestRate <=20){
+				c++;
+			}
+			else{
+				System.out.print("Invalid value. Enter valid interest rate:  ");
+				interestRate = input.nextDouble();
+			}
+		}
+		interestRate = interestRate/1200;
 		double presentValue = 0 ;
 		long j = 0 ;
 		while (j < yearsRetired){
@@ -43,7 +109,6 @@ public class financialCalculator {
 			k++;
 		}
 		monthlySavings = presentValue/interestMultiplier;
-		System.out.println(presentValue + "and" + monthlySavings);
-	}
-
+		System.out.println("You need to save " + presentValue + " in total, and " + monthlySavings + " a month");
+		}
 }
